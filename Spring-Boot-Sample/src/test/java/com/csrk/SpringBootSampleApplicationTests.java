17f483923e09ae901174fc0faa7f3de6fcdbfd1a
chandrasekhar <csrcg19@gmail.com>
2020-12-24 10:13:12 +0530

    spring boot app

---
+++ Spring-Boot-Sample/src/test/java/com/csrk/SpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csrk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
